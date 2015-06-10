64cfe91d7acd1657e7b6a6de0d517f9d770f0f3a
Ubuntu <ubuntu@ip-172-31-2-149.us-west-1.compute.internal>
2015-06-10 19:33:53 +0000

    kafa commit example 1

---
+++ kafka_code/simple_consumer.py
@@ -0,0 +1,14 @@
+from kafka import KafkaConsumer
+
+# To consume messages
+consumer = KafkaConsumer("filmon-topic2",
+                         group_id="my_group",
+                         bootstrap_servers=["localhost:9092"])
+for message in consumer:
+    # message value is raw byte string -- decode if necessary!
+    # e.g., for unicode: `message.value.decode('utf-8')`
+    print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                         message.offset, message.key,
+                                         message.value))
+
+kafka.close()
