54e61b41b7f9c92824885c39e941d5528fd06eac
Ubuntu <ubuntu@ip-172-31-2-149.us-west-1.compute.internal>
2015-06-12 02:38:28 +0000

    real time producer consumer from kafka works

---
+++ kafka_code/realtime_producer.py
@@ -0,0 +1,21 @@
+import os
+import sys
+from kafka import SimpleProducer, KafkaClient,KeyedProducer
+source_file='/home/ubuntu/truckBenefitMaximaization/data_generation/big_fake_outbound.csv'
+kafka = KafkaClient("localhost:9092")
+#this is to create topic if it doesn't exist
+#I still have to work on to check if it already existed !!!
+#os.system("/usr/local/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 --partitions 2 --topic real_time_data")
+producer = SimpleProducer(kafka)
+while True:
+	with open(source_file) as f:
+		for line in f:
+			producer.send_messages("real_time_data",line.rstrip())
+		f.close()
+#def ProduceData(Topic):
+	# To send messages synchronously
+	# Note that the application is responsible for encoding messages to type str
+#producer.send_messages("filmon_test_topic3", "some message creating topic")
+#
+
+
