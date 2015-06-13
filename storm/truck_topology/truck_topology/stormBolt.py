5bd7048ee9ae4bd797a4f9efeb135f4a156ad8f2
Ubuntu <ubuntu@ip-172-31-2-149.us-west-1.compute.internal>
2015-06-13 17:53:21 +0000

    storm with only spout working

---
+++ storm/truck_topology/truck_topology/stormBolt.py
@@ -0,0 +1,42 @@
+#Author: Filmon
+#storm bold program
+
+from pyleus.storm import SimpleBolt
+#import happybase
+#import json
+
+#connection = happybase.Connection('54.67.126.144') 
+
+#minuteTbl = connection.table('avlbl_Cabs')
+
+class firstBolt(SimpleBolt):
+#    OUTPUT_FIELDS = ['cabs']
+    
+#    def initialize(self):
+#        self.unoccCabs = {}        
+
+    def process_tuple(self, tup):
+        result, = tup.values
+	print result
+#        cabID, lat, lng, occ, timestamp = result.split(" ")
+        
+#        if (occ != '\N'): # check to ensure that there are no null values
+#            if int(occ) == 0:  
+#                self.unoccCabs[cabID] = {'c:lat':lat, 'c:lng':lng} # add unoccupied cab to table
+#            else:
+#	       if int(occ) == 1:
+#                  if (cabID in self.unoccCabs.keys()):
+#                      del self.unoccCabs[cabID]
+#                      minuteTbl.delete('StormData', columns=['c:' + cabID]) # remove cab from table if it is now occupied
+     
+#    def process_tick(self):
+#        cur_cabs = self.unoccCabs
+#	colDict = {}
+#        for key, val in cur_cabs.iteritems():
+#	    colDict['c:' + key] = json.dumps(val) # Add currently available cabs to HBase
+#        minuteTbl.put('StormData', colDict)
+
+if __name__ == '__main__':
+    firstBolt().run()
+
+
