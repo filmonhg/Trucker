67be26470df2aab8d516a0fa007690f4d24e8d74
Ubuntu <ubuntu@ip-172-31-2-149.us-west-1.compute.internal>
2015-06-14 22:03:28 +0000

    consumer stopped duplicating

--- kafka_code/consumer.py
+++ kafka_code/consumer.py
@@ -21,12 +21,14 @@ def consume_save(group,topic):
 	for message in messages:
 		tmp_save.write( message.message.value+"\n")
 #		print message.message.value+"\n"
+	kafka_consumer.commit() # inform zookeeper of position in the kafka queu
 	print ".... ... .. .."
 	print "Message from topic \"%s\" consumed \n" % topic
 #push the temporary fiele to hdfs
 def push_to_hdfs(tmp_file_path):
 	hadoop_dir="hadoop_dir_%s_%s" %(group,topic)
-	hdfs_file_name="hdfs_file.txt"
+	hdfs_file_name="hdfs_file.csv"
+#	hdfs_file_name="hdfs_file.txt"
 #	os.system("hdfs dfs -rmdir /%s_%s" % (hadoop_dir,topic))
 #	os.system("hdfs dfs -mkdir /%s" % hadoop_dir)
 	print "pushing to file \n"
