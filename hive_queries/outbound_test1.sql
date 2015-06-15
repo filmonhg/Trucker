2fef41ffbc59751e08736a46375abcb4d49dd68d
Ubuntu <ubuntu@ip-172-31-2-149.us-west-1.compute.internal>
2015-06-15 04:11:52 +0000

    group by works for hive sql

--- hive_queries/outbound_test1.sql
+++ hive_queries/outbound_test1.sql
@@ -7,9 +7,12 @@ STORED AS TEXTFILE LOCATION '/hadoop_dir_my_group_draft_data/';
 
 DESCRIBE draft_data;
 
+--set year=SPLIT(day,'/')
 INSERT OVERWRITE DIRECTORY '/hadoop_dir_my_group_draft_data_hive_output/draft_output_hive'
 ROW FORMAT DELIMITED FIELDS TERMINATED BY "\t" LINES TERMINATED BY "\n"
-SELECT origin_city, origin_state, trip, destination_city, destination_state
+--SELECT origin_city, origin_state, trip, destination_city, destination_state
+SELECT origin_state,destination_state,count(1)
 FROM draft_data
-WHERE destination_state='CA'
+GROUP BY origin_state,destination_state
+--WHERE origin_state='FL'
 
