be85120e8d49b86405fa644df4a7060d58a9f9fb
Ubuntu <ubuntu@ip-172-31-14-68.us-west-1.compute.internal>
2015-06-28 22:38:13 +0000

    kafka producer to hdfs finalized

--- kafka_code/realtime_producer_outbound.py
+++ kafka_code/realtime_producer_outbound.py
@@ -2,16 +2,16 @@ import os
 import sys
 import time
 from kafka import SimpleProducer, KafkaClient,KeyedProducer
-source_file='/home/ubuntu/Trucker/data_generation/test_data_simple.csv'
+source_file='/home/ubuntu/Trucker/data_generation/outbound_today.csv'
 kafka = KafkaClient("localhost:9092")
 #this is to create topic if it doesn't exist
 #I still have to work on to check if it already existed !!!
-#os.system("/usr/local/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 --partitions 2 --topic real_time_data")
+#os.system("/usr/local/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 --partitions 2 --topic real_time_data_outbound")
 producer = SimpleProducer(kafka)
 while True:
 	with open(source_file) as f:
 		for line in f:
-			producer.send_messages("real_time_data",line.rstrip())
+			producer.send_messages("real_time_data_outbound",line.rstrip())
 			time.sleep(0.5)
 		f.close()
 #def ProduceData(Topic):
